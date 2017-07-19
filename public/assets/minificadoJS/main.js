f6be393df493d4987d8baea41267a51f642fb3e7
Mone Rivas <mone.rivas@gmail.com>
2017-07-19 12:30:19 -0500

    Validación de campos y activación del boton en la plantalla view2

--- public/assets/minificadoJS/main.js
+++ public/assets/minificadoJS/main.js
@@ -1,2 +1,31 @@
-$(document).ready(function(){
-$('.carousel.carousel-slider').carousel({fullWidth: true});    });
+
+var cargarPagina = function () {
+    //Esta funcion inicializa el carousel de materialize
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
+    });    
+    /*validarTelefono();*/
+    
+    /*Si llamo la pura funcion y la inicializo no lo reconoce por lo tanto
+    desencadeno la funcion con un evento, en ambos elementos HTML para que se ejecute en ambos*/
+    $("#inputTelefono").keyup(validarTelefono);
+    $("#filled-in-box").change(validarTelefono);
+    
+
+};
+
+//si el lengt es diferente de cero y si es igual a 10
+/*Validaciones*/
+var validarTelefono = function () {
+    var botonContinuar = $("#botonContinuar");
+    var valorInputTelefono = $("#inputTelefono").val();
+    var checkBoxTerminos = $("#filled-in-box");
+
+    if (valorInputTelefono.length == 10 && checkBoxTerminos.prop("checked")) {
+        botonContinuar.removeClass("disabled");
+    } else {
+        botonContinuar.addClass("disabled");
+    }
+}
+
+$(document).ready(cargarPagina);
