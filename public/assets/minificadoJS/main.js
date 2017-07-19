a56cf5604e0cba9fb58d7921f5dbc1430bb9c6ef
Mone Rivas <mone.rivas@gmail.com>
2017-07-19 15:08:23 -0500

    iniciando la validacion del boton crear cuenta

--- public/assets/minificadoJS/main.js
+++ public/assets/minificadoJS/main.js
@@ -7,8 +7,11 @@ var cargarPagina = function () {
 
     /*Si llamo la pura funcion y la inicializo no lo reconoce por lo tanto
     desencadeno la funcion con un evento, en ambos elementos HTML para que se ejecute en ambos*/
-    $("#inputTelefono").keyup(validarTelefonoVIEW2);
     $("#filled-in-box").change(validarTelefonoVIEW2);
+    $("#inputTelefono").keyup(validarTelefonoVIEW2);
+    $("#valorInputNombre").keyup(validar3camposVIEW4);
+    $("#valorInputClave6").keyup(validar3camposVIEW4);
+
     /*$("#botonContinuar").click(hacerPostAPI);*/
 };
 
@@ -23,6 +26,21 @@ var validarTelefonoVIEW2 = function () {
     } else {
         botonContinuar.addClass("disabled");
     }
+}
+
+var validar3camposVIEW4= function(){
+    var valorInputNombre =$("#inputNombre").val();
+    var valorInputMail =$("#inputMail").val();
+    var valorInputClave6 =$("#inputClave6").val();
+    var botonCrearCuenta= $("botonCrearCuenta");
+    
+    
+    if(valorInputNombre != null && valorInputClave6.length == 6 /*meter condicional del mail*/  ){
+       
+       botonCrearCuenta.removeClass("disabled");
+       }else{
+       botonCrearCuenta.addClass("disabled");
+       }
 }
 /*----------Fin Validaciones-----------*/
 
