3fb3c70354c3ac0573e32933965d89ea0933729b
Mone Rivas <mone.rivas@gmail.com>
2017-07-20 22:48:04 -0500

    funcionalidad y maquetacion completa de la pantalla 3,4 y 5

--- public/assets/minificadoJS/main.js
+++ public/assets/minificadoJS/main.js
@@ -10,14 +10,18 @@ var cargarPagina = function () {
     $("#filled-in-box").change(validarTelefonoVIEW2);
     $("#inputTelefono").keyup(validarTelefonoVIEW2);
 
-     $("#inputNombre").keyup(validar3camposVIEW4);
-     $("#inputMail").keyup(validar3camposVIEW4);    
-     $("#inputClave6").keyup(validar3camposVIEW4);
+    $("#inputNombre").keyup(validar3camposVIEW4);
+    $("#inputMail").keyup(validar3camposVIEW4);
+    $("#inputClave6").keyup(validar3camposVIEW4);
 
-    $("#botonContinuar").click(hacerPostAPI);
+    $("#botonContinuar").click(hacer1erPostTelAPI); //En pantalla2 activacion de la funcion que hace
     $("#inputCodigoTelefono").keyup(validarCodigoDelTelefono);
 
-    
+
+    $("#botonCrearCuenta").click(hacer3erPostDe3camposAPI); //En pantalla4 activacion de la funcion que hace el 3 post a la API
+
+
+
 
 };
 
@@ -43,7 +47,7 @@ var validar3camposVIEW4 = function () {
     var botonCrearCuenta = $("#botonCrearCuenta");
 
 
-    if (valorInputNombre.length != 0 && valorInputMail.length !=0 && valorInputClave6.length == 6) {
+    if (valorInputNombre.length != 0 && valorInputMail.length != 0 && valorInputClave6.length == 6) {
 
         botonCrearCuenta.removeClass("disabled");
     } else {
@@ -82,16 +86,15 @@ var api = {
 
 }
 
-var hacerPostAPI = function () {
+var hacer1erPostTelAPI = function () {
     //esto es para que el formulario no haga su accion por default que es enviar cosas
     var valorInputTelefono = $("#inputTelefono").val();
     $.post(api.urlRegisterNumbers, {
         "phone": valorInputTelefono,
         "terms": true
     }, function (response) {
-        console.log(response.data.code);
-        /*Guardo en localStorage (que es como una cookie pero que no conceta con el servidor, solo guarda data en el navegador) 
-                el primer parametro que es el nombre de la data
+        console.log(response.data.code);/*Estoy accediendo a la propiedad data.code del JSON que me respondio la API*/
+        /*Guardo en localStorage (que es como una cookie pero que no conceta con el servidor, solo guarda data en el navegador) el primer parametro que es el nombre de la data
                 y el segundo parametro que es la data que quiero que guarde. como propiedad de un objeto y su valor.*/
         localStorage.setItem("phone", response.data.phone);
         localStorage.setItem("code", response.data.code);
@@ -102,17 +105,44 @@ var hacerPostAPI = function () {
 };
 
 var codigo21seg = function () {
-    $.post(api.urlResendCode, {//hago un post a la url siguiente endpoint 
-        "phone": localStorage.getItem("phone")/*En la linea 37 del arcgivo users.js de la API viene que para hacer un post a este endpoint (req) hay que enviar la propiedad phone (y su valor) al JSON. Le digo que a la propiedad phone le meta el valor que ya tenía guardado en localStorage porque no le debo volver a pedir al usuario el telefono(ademas de que eso me daria error porque ya esta registrado ese numero)*/
+    $.post(api.urlResendCode, { //hago un post a la url siguiente endpoint 
+        "phone": localStorage.getItem("phone") /*En la linea 37 del arcgivo users.js de la API viene que para hacer un post a este endpoint (req) hay que enviar la propiedad phone (y su valor) al JSON. Le digo que a la propiedad phone le meta el valor que ya tenía guardado en localStorage porque no le debo volver a pedir al usuario el telefono(ademas de que eso me daria error porque ya esta registrado ese numero)*/
     }, function (response) {
-        console.log(response);/*veo que respuesta me da la API, me da un JSON con dos propiedades phone y message*/
-        localStorage.setItem("code", response.data);/*Sobre escribo la propiedad code del localStorage (con el valor de la propiedad data porque en este JSON de este endpoint el code no se llama code, como en el pasado, se llama data ) para invalidar el codigo anterior y que quede este nuevo guardado*/
-        console.log(response.data);/*Hago console.log de como queda la propiedad data (con el valor de code)del JSON de este endpoint */
+        console.log(response); /*veo que respuesta me da la API, me da un JSON con dos propiedades phone y message*/
+        localStorage.setItem("code", response.data); /*Sobre escribo la propiedad code del localStorage (con el valor de la propiedad data porque en este JSON de este endpoint el code no se llama code, como en el pasado, se llama data ) para invalidar el codigo anterior y que quede este nuevo guardado*/
+        console.log(response.data); /*Hago console.log de como queda la propiedad data (con el valor de code)del JSON de este endpoint */
         /**/
         alert(response.data);
     });
-
 }
+
+
+var hacer3erPostDe3camposAPI = function (e) {
+    /*Esta funcion se ejecuta en la pantalla4 (view4)*/
+    
+    e.preventDefault();/*Este prevenDefault es porque el boton que activa esta funcion esta dentro de un form y tiene un type submit por default, lo que hace que se recargue la pagina una y otra vez y yo no quiero eso, quiero que me mande a la siguiente pagina*/
+   
+    var valorInputNombre = $("#inputNombre").val();
+    var valorInputMail = $("#inputMail").val();
+    var valorInputClave6 = $("#inputClave6").val();
+/*
+    var botonCrearCuenta = $("#botonCrearCuenta");
+*/
+    $.post(api.urlCreateUser, { /*Basada en el router.post('/createUser') de la linea 64 del archivo users.js los requiests de este endpoint son las propiedades phone, name, email y password de su JSON. El response de este endpoint son las propiedades success, message, data se du JSON*/
+        "phone": localStorage.getItem("phone"),
+        "name": valorInputNombre,
+        "email": valorInputMail,
+        "password": valorInputClave6
+    }, function (response) {
+        console.log( response.success); 
+        console.log( response.message); 
+        console.log( response.data);
+        if(response.success == true){
+        location.href = "view5.html"
+            console.log("funciina el IF");
+           }
+    });
+};
 /*----------Fin API------------*/
 
 
